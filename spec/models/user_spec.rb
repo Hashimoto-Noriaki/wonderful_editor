731b634a708190e77d98df3c6d21ef536679d904
橋本範昭 <nori.hasimo.89@gmail.com>
2021-07-28 02:53:34 +0900

    modelのテストをRubocopで静的コード解析

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-   context "nameを指定した時"do
-    it "userが作られる"do
-    user = User.new(name:"pochi",email:"pochi@example.com",password:"pochi")
-    expect(user.valid?).to eq false
+  context "nameを指定した時" do
+    it "userが作られる" do
+      user = User.new(name: "pochi", email: "pochi@example.com", password: "pochi")
+      expect(user.valid?).to eq false
     end
   end
-  
 
-  context "nameを指定していない時"do
-    it "user作成に失敗する"do
-    user = User.new(name:"nil",email:"pochi@example.com",password:"pochi")
-    expect(user.invalid?).to eq true
+  context "nameを指定していない時" do
+    it "user作成に失敗する" do
+      user = User.new(name: "nil", email: "pochi@example.com", password: "pochi")
+      expect(user.invalid?).to eq true
     end
   end
 
   context "同じnameとemailが存在しない時" do
-    it "userが作れれる"do
-    user = User.new(name:"pochi",email:"pochi",password:"pochi")
-    expect(user).to be_invalid
+    it "userが作れれる" do
+      user = User.new(name: "pochi", email: "pochi", password: "pochi")
+      expect(user).to be_invalid
     end
   end
 
   context "同じnameとemailが存在している時" do
-    fit "user作成に失敗する"do
-    # binding.pry
-    # User.create!(name:"pochi",email:"pochi",password:"pochi")
-    user = User.new(name:"pochi",email:"pochi@example.com",password:"pochi")
-    expect(user).to be_invalid
+    it "user作成に失敗する" do
+      # binding.pry
+      # User.create!(name:"pochi",email:"pochi",password:"pochi")
+      user = User.new(name: "pochi", email: "pochi@example.com", password: "pochi")
+      expect(user).to be_invalid
     end
   end
 end
