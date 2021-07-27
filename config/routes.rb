cea46d3ecd96bae65ad16f6c894f2276f4600677
橋本範昭 <nori.hasimo.89@gmail.com>
2021-07-28 03:38:47 +0900

    Rubocopが通らないためroutes.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-  mount_devise_token_auth_for "User", at: "auth"
-  get ':users_id/articles', to:'articles#get'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      # mount_devise_token_auth_for "User", at: "auth"
+      # get ":users_id/articles", to: "articles#get"
+      resources :articles
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
   end
 end
-end
-
 
 # get ':area_id/:prefecture_id/cities', to:'cities#get'
 
@@ -18,5 +18,3 @@ end
 # 補足
 
 # /api/v1/articles で CRUD 処理ができるような endpoint が生えるよう、routes.rb を修正していきましょう！
-
-
