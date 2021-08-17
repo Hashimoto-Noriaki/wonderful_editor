5186254af090fad1049b99f9e638e0c7ba1c9e09
橋本範昭 <nori.hasimo.89@gmail.com>
2021-08-17 22:59:45 +0900

    記事詳細に必要なAPI実装

--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlePreviewSerializerSerializer < ActiveModel::Serializer
   attributes :id, :title, :body, :updated_at
-  #  render json: title,body,updated_at
+  belongs_to :users, title:,body:,
   # has_many  :comments,:user
 end
