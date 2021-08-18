949dc9b69c00337ddc9bdcb46a4a8f2728c01447
橋本範昭 <nori.hasimo.89@gmail.com>
2021-08-18 23:41:21 +0900

    rubocopで修正

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -19,18 +19,17 @@ module Api::V1
     def updated
       article = Article.find(params[:id])
 
-    #   # DBに登録できた場合
-   article.update(article_params)
-   render json: article,each_serializer: Api::V1::ArticlePreviewSerializer
-    #       # 記事ページに遷移する
-         redirect_to article
+      #   # DBに登録できた場合
+      article.update!(article_params)
+      render json: article, each_serializer: Api::V1::ArticlePreviewSerializer
+      #       # 記事ページに遷移する
+      redirect_to article
     end
-    
-    
+
     def destroy
-    article = Article.find(params[:id])
-        if article.user_id == current_user.id
-          article.destroy
+      article = Article.find(params[:id])
+      if article.user_id == current_user.id
+        article.destroy!
       end
     end
 
