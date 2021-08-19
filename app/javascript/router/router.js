2797db09b7c8a2ee90132cd9c8aee1d7fb2cbb5c
橋本範昭 <nori.hasimo.89@gmail.com>
2021-08-19 16:20:28 +0900

    フロントエンドの部分を導入

---
+++ app/javascript/router/router.js
@@ -0,0 +1,17 @@
+import Vue from "vue";
+import Router from "vue-router";
+import ArticleList from "../components/ArticleList.vue";
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: "history",
+  routes: [
+    //ルーティングの設定
+    {
+      path: "/",
+      component: ArticleList,
+    },
+  ],
+});
+export default router;
