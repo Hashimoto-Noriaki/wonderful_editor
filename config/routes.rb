eb837dac555d704d3a5adf463471ca0a034e5b64
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-05 01:51:47 +0900

    フロントを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  # root to: "home#index"
+
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
-      # get ":users_id/articles", to: "articles#get"
+      # controllers: {
+      #   registrations: 'auth/registrations'
+      #   }
       resources :articles
-      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     end
   end
 end
-
-# get ':area_id/:prefecture_id/cities', to:'cities#get'
-
-# タスク内容
-
-# API の endpoint （どんな URL アクセスにするか）を設定しよう。
-# （ endpoint は /api/v1 から始まることとする ）
-# 補足
-
-# /api/v1/articles で CRUD 処理ができるような endpoint が生えるよう、routes.rb を修正していきましょう！
