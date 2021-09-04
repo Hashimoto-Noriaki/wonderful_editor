6c46cb7d4ac9a655d6036be1ebd31251a5b47f17
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-05 01:23:59 +0900

    記事一覧と詳細のテスト

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,19 +1,38 @@
 require "rails_helper"
 
-RSpec.describe "Articles", type: :request do
-  describe "GET /articles" do
-    subject {get(api_v1_articles_path)}
-
-    before { create_list(:article,3)}
-    fit "記事一覧が取得できる" do
+RSpec.describe "Api::V1::Articles", type: :request do
+  describe "GET /Article" do
     
-      res = JSON.parse(response.body)
-      expect(res[0].length).to eq 3
+    subject { get(api_v1_articles_path) }
+    let(:article){create_list(:article,3)}
+   it "記事一覧が取得できる" do
+      subject
+      res =  JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+    end
+  end
 
-      expect(response.body).to have_http_status(200)
 
-    
+  describe "GET /Articles/:id" do
+    context "指定したidの記事が存在する時"do
+    subject { get(api_v1_articles_path(article_id)) }
+    let(:article){create(:article)}
+    let(:article_id){article.id}
+    fit "見たい記事詳細を取得できる" do
+    #expect(response).to have_http_status(200)
+  end
 end
 end
-
+     context "指定したidの記事が存在しない時" do
+      let(:article_id){10000000000000000000}
+      fit "記事が見つからない" do
+        subject
+    end
+    end
 end
+
+  
+
+
+
+# end
