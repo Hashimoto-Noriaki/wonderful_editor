8b663c7817cbfa84b8d526647b29448030b14487
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-14 17:56:32 +0900

    新規登録機能　

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:trackable
+         :recoverable, :rememberable, :validatable, :trackable
   include DeviseTokenAuth::Concerns::User
 
   validates :name, presence: true
