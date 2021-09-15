810bb041809e82fae0aac133c3a1b8dc6e925289
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-16 05:35:54 +0900

    新規登録テスト中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,38 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
 end
+
+# require "rails_helper"
+
+# RSpec.describe "Api::V1::Articles", type: :request do
+#   describe "GET /Article" do   #index
+#     subject { get(api_v1_articles_path) }
+
+#     let(:article) { create_list(:article, 3) }
+#     it "記事一覧が取得できる" do
+#       subject
+#       # binding.pry
+#        res = JSON.parse(response.body)
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+
+#   describe "GET /Articles/:id" do  #show
+#     context "指定したidの記事が存在する時" do
+#       subject { get(api_v1_articles_path(article_id)) }
+
+#       let(:article) { create(:article) }
+#       let(:article_id) { article.id }
+#       fit "見たい記事詳細を取得できる" do
+#       expect(response).to have_http_status(200)
+#       end
+#     end
+#   end
+
+#   context "指定したidの記事が存在しない時" do
+#     let(:article_id) { 10_000_000_000_000_000_000 }
+#     fit "記事が見つからない" do
+#       subject
+#     end
+#   end
+# end
