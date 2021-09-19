e30069db87949f440794a0b51aea989c893f0534
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-19 22:23:38 +0900

    新規登録テスト

--- spec/requests/api/v1/auth/registrations_spec.rb
+++ spec/requests/api/v1/auth/registrations_spec.rb
@@ -1,16 +1,38 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Auth::Registrations", type: :request do
-  describe "POST /api/v1/auth" do
+  describe "POST /V1/auth" do
     subject { post(api_v1_user_registration_path, params: params) }
-
-    # context "パラメーターを送信する時" do
-    let(:params) { attributes_for(:user) }
-    it "新規登録できる" do
-      # expect { subject }.to change { User.count }.by(0)
-      # expect(response).to have_http_status(:unprocessable_entity)
-      # JSON.parse(response.body)
-      # end
+    context "パラメーターを送信する時" do
+      binding.pry
+  let(:params) {attributes_for(:user,name: "ぽこぽぽぽ")}
+      binding.pry
+    fit "新規登録できる" do
+      expect { subject }.to change { User.count }.by(1)
+       expect(response).to have_http_status(200)
+       res = JSON.parse(response.body)
+     end
     end
   end
+
 end
+
+
+
+
+
+
+
+      #  describe "POST /api/v1/sign_in" do
+      #   subject { post(api_v1_user_registration_path, params: params) }
+      #    context "" do
+
+      #     it "" do
+
+
+      #     end
+      #     end
+
+      #   end
+
+# end
