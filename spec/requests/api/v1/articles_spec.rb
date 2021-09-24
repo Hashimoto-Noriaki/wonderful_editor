2ea4228d6a4e5742527725f04fe6422c394e29bc
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-25 05:20:24 +0900

    記事詳細テストはコメントアウト

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -16,23 +16,23 @@ RSpec.describe "Api::V1::Articles", type: :request do
     end
   end
 
-  describe "GET /Articles/:id" do # show
-    context "指定したidの記事が存在する時" do
-      subject { get(api_v1_articles_path(article_id)) }
+  # describe "GET /Articles/:id" do # show
+  #   context "指定したidの記事が存在する時" do
+  #     subject { get(api_v1_articles_path(article_id)) }
 
-      let(:article) { create(:article) }
-      let(:article_id) { article.id }
-      it "見たい記事詳細を取得できる" do
-        # binding.pry
-        expect(response).to have_http_status(:ok)
-      end
-    end
+  #     let(:article) { create(:article) }
+  #     let(:article_id) { article.id }
+  #     it "見たい記事詳細を取得できる" do
+  #       # binding.pry
+  #       expect(response).to have_http_status(:ok)
+  #     end
+  #   end
 
-    context "指定したidの記事が存在しない時" do
-      let(:article_id) { 10_000_000_000_000_000_000 }
-      it "記事が見つからない" do
-        subject
-      end
-    end
-  end
+  #   context "指定したidの記事が存在しない時" do
+  #     let(:article_id) { 10_000_000_000_000_000_000 }
+  #     it "記事が見つからない" do
+  #       subject
+  #     end
+  #   end
+  # end
 end
