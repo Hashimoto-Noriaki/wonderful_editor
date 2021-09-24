c432d3aed443924dfa189f2b1f6df32fde1c435d
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-25 05:01:33 +0900

    記事一覧テスト

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,38 +1,42 @@
-module Api::V1
-  class ArticlesController < BaseApiController
-    def index
-      articles = Article.order(updated_at: :desc)
-      render json: articles, each_serializer: Api::V1::ArticlePreviewSerializer
-    end
-
-    def show
-      article = Article.find(params[:id])
-      render json: article, serializer: Api::V1::ArticleSerializer
-    end
-
-    def create
-      article = current_user.article.create!(article_params)
-      render json: article, serializer: Api::V1::ArticleSerializer
-    end
-
-    def updated
-      article = current_user.articles.find(params[:id])
-      #   # DBに登録できた場合
-      article.update!(article_params)
-      render json: article, serializer: Api::V1::ArticleSerializer
-    end
-
-    def destroy
-      article = current_user.article.find(params[:id])
-      article.destroy!
-    end
-
-    private
-
-      def article_params
-        params.require(:article).permit(:title, :body)
-      end
+ module Api::V1
+class ArticlesController < BaseApiController
+  def index
+    articles = Article.order(updated_at: :asc)
+    render json: articles,each_serializer: Api::V1::ArticlePreviewSerializer
   end
+
+
+def show
+  article = Article.find(params[:id])
+  render json: article
+  # , serializer: Api::V1::ArticleSerializer
+end
+
+# def create
+#   article = current_user.article.create!(article_params)
+#   render json: article, serializer: Api::V1::ArticleSerializer
+# end
+
+# def updated
+#   article = current_user.articles.find(params[:id])
+#   #   # DBに登録できた場合
+#   article.update!(article_params)
+#   render json: article, serializer: Api::V1::ArticleSerializer
+# end
+
+# def destroy
+#   article = current_user.article.find(params[:id])
+#   article.destroy!
+# end
+
+# private
+
+#   def article_params
+#     params.require(:article).permit(:title, :body)
+#   end
+
+end
+
 end
 
 # モデル.order(:キー名 [ :並び順])
