51ad1b8da54ec8666e922484ce0bfc804feacd11
橋本範昭 <nori.hasimo.89@gmail.com>
2021-09-25 18:36:31 +0900

    き

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -38,27 +38,3 @@ end
 # モデル.order(:キー名 [ :並び順])
 
 # format.json { render json: @article, root: false }
-
-describe "GET /articles/:id" do # show
-  subject { get(api_v1_article_path(article_id)) }
-
-  context "指定したidの記事が存在する時" do
-    let(:article) { create(:article) }
-    let(:article_id) { article.id }
-    it "見たい記事詳細を取得できる" do
-      subject
-      res = JSON.parse(response.body)
-      expect(res["title"]).to eq article.title
-      expect(res["body"]).to eq article.body
-      expect(res["user"]["id"]).to eq article.user.id
-      expect(response).to have_http_status(:ok)
-    end
-  end
-
-  context "指定したidの記事が見つからない時" do
-    let(:article_id) { 100_000_000_000_000_000 }
-    it "記事が見つからない" do
-      expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-  end
-end
